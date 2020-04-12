1315d7c813e9b41cde39f96e25074f215cf71799
Mehul Golania <mehul.osplabs@gmail.com>
2020-04-12 15:41:06 +0530

    title updated

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render(){
     return <div>
-      <h1>React Boilerplate</h1>
+      <h1>React Custom Boilerplate</h1>
     </div>
   }
 }
