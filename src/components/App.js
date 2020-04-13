560fe7d13cb60b243d7b83e3b4f3f987b0d8ece4
Mehul Golania <mehul.osplabs@gmail.com>
2020-04-13 11:51:47 +0530

    Added the demo image

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import demo from '../images/demo.jpg';
 
 class App extends Component {
   render(){
     return <div>
       <h1>React Custom Boilerplate</h1>
+
+      <img src={demo} alt="demo image" />
     </div>
   }
 }
